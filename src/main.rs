aa52127924fae535f8712e6ebd91b0fffdff05c3
John Lockwood <jlockwood@verticalrelevance.com>
2023-02-23 11:40:45 -0500

    Begin modifying clap demo to be wk instead

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
@@ -8,25 +7,24 @@ struct Cli {
     /// Optional name to operate on
     name: Option<String>,
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
     command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
+    /// adds a task
+    Add {
+        /// what you're doing
         #[arg(short, long)]
-        list: bool,
+        name: String,
+        #[arg(short, long)]
+        duration: Option<u16>
+        
+    },
+    /// shows tasks today including current
+    Today {
+
     },
 }
 
@@ -38,30 +36,23 @@ fn main() {
         println!("Value for name: {name}");
     }
 
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
+        Some(Commands::Add {name, duration}) => {
+            let mut elapsed : u16 = 25;
+            if let Some(duration) = duration {
+                elapsed = *duration;
             }
+            
+            println!("Adding a task {name} with duration {elapsed} minutes.");        
+        },
+        Some(Commands::Today {}) => {
+            println!("Today you're doing great!");
         }
-        None => {}
+        None => {
+            println!("No command found!");
+        }
     }
 
     // Continued program logic goes here...
